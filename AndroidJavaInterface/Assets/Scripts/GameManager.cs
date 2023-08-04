e0598fc6b41862bfb9e2c822d2a7b53ce5571465
NoemieGrf <epigrf@gmail.com>
2023-08-04 16:54:46 +0800

    proj urp

---
+++ AndroidJavaInterface/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
